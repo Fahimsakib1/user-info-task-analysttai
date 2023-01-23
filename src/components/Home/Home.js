5455a0616669454e94f5743e41f7325203b25bd3
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2023-01-23 23:43:21 +0600

    pagination design added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,6 +26,7 @@ const Home = () => {
         })
     }, [])
 
+    
 
 
     return (
@@ -46,9 +47,30 @@ const Home = () => {
                             userInfo.map(user => <UserDetails user={user} key={user.id}></UserDetails>)
                         }
                     </tbody>
-                    
+
                 </table>
+
+                <div className="flex justify-center space-x-1 text-gray-100 mb-12">
+                    <button title="previous" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md bg-gray-700 border-gray-800">
+                        <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
+                            <polyline points="15 18 9 12 15 6"></polyline>
+                        </svg>
+                    </button>
+                    
+                    <button type="button" title="Page 1" className="inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md hover:bg-rose-500 bg-gray-900 border-white ">1</button>
+                    <button  type="button" className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md hover:bg-rose-500 bg-gray-900 border-white" title="Page 2">2</button>
+                    <button type="button" className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md hover:bg-rose-500 bg-gray-900 border-white" title="Page 3">3</button>
+                    <button type="button" className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md hover:bg-rose-500 bg-gray-900 border-white" title="Page 4">4</button>
+                    
+                    <button title="next" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md bg-gray-700 border-gray-800 ">
+                        <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
+                            <polyline points="9 18 15 12 9 6"></polyline>
+                        </svg>
+                    </button>
+                </div>
             </div>
+
+
 
         </div>
     );
